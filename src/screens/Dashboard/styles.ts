7ea8c9d6e75265a5fe17e89a77bb051a6d11bbf2
marcos-dev14 <mprogramador.10@gmail.com>
2021-06-24 13:28:53 -0300

    Adicionado o styled-components e o tema global da aplicação

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: ${({ theme }) => theme.colors.background};
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-size: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
